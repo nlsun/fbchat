8288aad1d6a66cecd10569d2f865fe28e4f1744d
Nicholas Sun <olassun2@gmail.com>
2013-06-28 13:34:37 -0700

    cleaned main

--- main.js
+++ main.js
@@ -7,18 +7,3 @@ chrome.tabs.create({
         }
     });
 });
-
-/*function openChat(url) {
-    var newWindow = window.open(url);
-    document.addEventListener('DOMContentLoaded', function () {
-      newWindow.document.getElementById('pagelet_bluebar').innerHTML='';
-    });
-    window.open(url);
-    //chrome.tabs.executeScript(null, {file: "removeContent.js"});
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-  //openChat("http://www.facebook.com/");
-  chrome.tabs.executeScript(null, {file: "removeContent.js"});
-});
-*/
